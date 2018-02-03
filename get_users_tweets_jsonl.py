b65c7aeb68fc36c6d96d692fe7d5111ab42b894a
timor <saverymax@gmail.com>
2018-02-03 08:31:55 -0500

    update

--- get_users_tweets_jsonl.py
+++ get_users_tweets_jsonl.py
@@ -3,6 +3,7 @@ from tweepy import OAuthHandler
 import json
 import sys
 import os
+import config
 """
 This program generates a json file that contains the specificed number of
 tweets from a specified user, or the concatenated dictionary of tweets from multiple users (if called by get_tweets.sh).
@@ -12,13 +13,8 @@ To properly run the script, enter python user_tweet.py screen name for twitter u
 This file is called by get_tweets.sh. However, it can also be run independently in the command line. However, there is currently no way to call from the command line the user/s of interest 
 """
 
-consumer_key = '7GSXtxZvVxgMqc7zQms88Rn1A'
-consumer_secret = 'DmiMJPPFbzPc0fTykPl97JIL4J0DeU69hfXrzPvw1vu2xAoYZL'
-access_token = '564338092-dOtS6UWrFzLS3WDbA3YuqaEeYKMnCnMCA6kH3dPH'
-access_secret = 'gOPv6wJ2xXwJBP4jU6vFpQ99Iyla5rZkLbGzYH29NAS5C'
-
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+auth = OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_secret)
 
 api = tweepy.API(auth)
 # Download users tweets
