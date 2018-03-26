e6b5037b606d11a8fd7009e22630eba1a2d3520d
timor <saverymax@gmail.com>
2018-03-26 19:18:24 -0400

    time series update

--- trump/time_series_plotly.py
+++ trump/time_series_plotly.py
@@ -1,4 +1,4 @@
-import plotly_credentials
+import plotly_credentials # private file with my plotly login
 import plotly.plotly as py
 import plotly.tools as tls
 import plotly.graph_objs as go
@@ -9,9 +9,9 @@ from sklearn.preprocessing import scale
 import os
 
 def time_series(tweet_dataframe):
-    """Using data from pandas dataframe, create a time series plot of retweets, likes, polarity and subjectivity"""
+    """Using data from pandas dataframe, create a time series plot of the president's retweets, likes, polarity and subjectivity"""
    
-    py.sign_in(plotly_credentials.username,plotly_credentials.plotly_password)
+    py.sign_in(plotly_credentials.username,plotly_credentials.plotly_password) # Login to plotly
 
     # create individual plotly plotting objects. These will be added to subplots below
     likes_data = go.Scatter(
@@ -21,42 +21,51 @@ def time_series(tweet_dataframe):
                             mode = 'markers', 
                             text = tweet_dataframe['filtered_text']
                             ) # default is a line plot
+
     retweets_data = go.Scatter(
                             x = tweet_dataframe['date'], 
                             y = tweet_dataframe['retweets'].values, 
                             name = 'Retweets',
                             mode = 'markers', 
                             text = tweet_dataframe['filtered_text']
-                            ) # default is a line plot
+                            )
+
+    # For senitment analysis see description given by Tweepy documentation:
+    # "The polarity (positivity and negativity) score is a float within the range [-1.0, 1.0]. The subjectivity is a float within the range [0.0, 1.0] where 0.0 is very objective and 1.0 is very subjective."
     polarity_data = go.Scatter(
                             x = tweet_dataframe['date'], 
                             y = tweet_dataframe['polarity'].values, 
                             name = 'Polarity',
-                            mode = 'markers', 
-                            text = tweet_dataframe['filtered_text']) # default is a line plot
+                            mode = 'markers',
+                            text = tweet_dataframe['filtered_text']) 
+
     sentiment_data = go.Scatter(
                             x = tweet_dataframe['date'], 
                             y = tweet_dataframe['subjectivity'].values, 
                             name = 'Sentiment',
                             mode = 'markers', 
-                            text = tweet_dataframe['filtered_text']) # default is a line plot
+                            text = tweet_dataframe['filtered_text']) 
 
     # data_1 = [likes_data, retweets_data] # Use these to plot two data sets on one graph
     # data_2 = [polarity_data, sentiment_data] 
     # plot_url = py.plot(data_1)
 
-    fig = tls.make_subplots(rows=2, cols=1, subplot_titles = ("Likes and Retweets", "Polarity and Subjectivity"))
-    fig['layout'].update(title="Time series of trumps tweets")
+    fig = tls.make_subplots(rows=2, cols=1, subplot_titles = ('Likes and Retweets', 'Polarity and Subjectivity'))
+    fig['layout'].update(title='Time series of trumps tweets')
+    fig['layout']['xaxis1'].update(title='Date')
+    fig['layout']['yaxis1'].update(title='Likes and Retweet Frequency')
+    fig['layout']['xaxis2'].update(title='Date')
+    fig['layout']['yaxis2'].update(title='Polarity and Subjectivity Score')
     # fig = go.Figure(data = data, layout = layout) # can use this line for more customization. See below url.
     # https://plot.ly/python/time-series/
-    fig.append_trace(retweets_data, 1, 1)
+    fig.append_trace(retweets_data, 1, 1) # 1,1 refers to location of subplot in fig object
     fig.append_trace(likes_data, 1, 1)
     fig.append_trace(polarity_data, 2, 1)
     fig.append_trace(sentiment_data, 2, 1)
     plot_url = py.plot(fig) 
 
 def normalize_data(column_name):
-    """Normalize columns in the tweet data, especially since polarity and subjectivity and are two different scales."""
+    """Normalize columns in the tweet data, particularly because polarity and subjectivity and are on two different scales."""
 
     normalized_data = pd.Series(scale(column_name))
     return(normalized_data)
@@ -64,7 +73,7 @@ def normalize_data(column_name):
 path = '/home/timor/Documents/Git/Twitter-Mining/trump/trumps_tweets'
 os.chdir(path)
 
-tweet_data = pd.read_csv("converted_tweets_sentiment.tsv", sep = '\t', parse_dates=[2]) # Neccessary to parse_dates to put in format that can be read and plotted.
+tweet_data = pd.read_csv('converted_tweets_sentiment.tsv', sep = '\t', parse_dates=[2]) # Neccessary to parse_dates to put in format that can be read and plotted.
 #tweet_data = normalize_data(tweet_data) # this isn't working because can't do math with zeros
 tweet_data['polarity'] = normalize_data(tweet_data['polarity'])
 tweet_data['subjectivity'] = normalize_data(tweet_data['subjectivity'])
