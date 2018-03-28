c321948620b50658490868d1dd4208c6cc25214c
timor <saverymax@gmail.com>
2018-03-28 19:00:40 -0400

    test

---
+++ trump/config.py
@@ -0,0 +1,5 @@
+
+consumer_key = '7GSXtxZvVxgMqc7zQms88Rn1A'
+consumer_secret = 'DmiMJPPFbzPc0fTykPl97JIL4J0DeU69hfXrzPvw1vu2xAoYZL'
+access_token = '564338092-dOtS6UWrFzLS3WDbA3YuqaEeYKMnCnMCA6kH3dPH'
+access_secret = 'gOPv6wJ2xXwJBP4jU6vFpQ99Iyla5rZkLbGzYH29NAS5C'
