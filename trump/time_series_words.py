e8f485269324dd00e0a40ddd3667deee15fddb02
saverymax <saverymax@gmail.com>
2018-06-08 17:36:16 -0400

    update trump

--- trump/time_series_words.py
+++ trump/time_series_words.py
@@ -4,17 +4,41 @@ import matplotlib.pyplot as plt
 import os
 
 def time_series(tweet_dataframe):
-    """Using data from pandas dataframe, create a time series plot of retweets, likes, polarity and subjectivity"""
+    """
+    Create a time series plot of word frequencies, as generated in ipynb  
+    Would have made this plot iteratively if I had know I was going to do so many words :((((
+    """
     
     first_date = '2018-03-18'
     dates = pd.date_range(start = first_date, periods = 11, freq = 'W') 
 
-    #investigation = pd.Series(tweet_dataframe.loc['investigation'].values)
     china = pd.Series(tweet_dataframe.loc[:, 'china'].values, index = dates)
     korea = pd.Series(tweet_dataframe.loc[:, 'korea'].values, index = dates)
     guns = pd.Series(tweet_dataframe.loc[:, 'guns'].values, index = dates)
     immigration = pd.Series(tweet_dataframe.loc[:, 'immigration'].values, index = dates)
+    trade = pd.Series(tweet_dataframe.loc[:, 'trade'].values, index = dates)
+    russia = pd.Series(tweet_dataframe.loc[:, 'russia'].values, index = dates)
+    wall = pd.Series(tweet_dataframe.loc[:, 'wall'].values, index = dates)
+    fake = pd.Series(tweet_dataframe.loc[:, 'fake'].values, index = dates)
+    fbi = pd.Series(tweet_dataframe.loc[:, 'fbi'].values, index = dates)
+    comey = pd.Series(tweet_dataframe.loc[:, 'comey'].values, index = dates)
+    great = pd.Series(tweet_dataframe.loc[:, 'great'].values, index = dates)
    
+   
+    colors =[
+            '#C03028',
+            '#F08030',
+            '#6890F0',
+            '#78C850',
+            '#A890F0',
+            '#A040A0',
+            '#F8D030',
+            '#E0C068',
+            '#A8A878',
+            '#B8A038',
+            '#800000',
+            ]
+     
     time_series_figure, axis1 = plt.subplots(1,1, figsize = (20,10), sharex = True)
 
     time_series_figure.suptitle("Word frequencies over time", fontsize = 20)
@@ -23,12 +47,19 @@ def time_series(tweet_dataframe):
     axis1.set_ylabel('Frequency of terms', fontsize = 20)
     axis1.tick_params(labelsize = 15)
     
-    #axis1.plot(investigation, color = "b", label = "investigation")
-    axis1.plot(china, color = "grey", linewidth = 7, label = "China")
-    axis1.plot(korea, color = "r", linewidth = 7, label = "Korea")
-    axis1.plot(guns, color = "g", linewidth = 7, label = "guns")
-    axis1.plot(immigration, color = "y", linewidth = 7,label = "immigration")
-    
+               
+    axis1.plot(china, color = colors[0], linewidth = 7, label = "China")
+    axis1.plot(korea, color = colors[1], linewidth = 7, label = "Korea")
+    axis1.plot(guns, color = colors[2], linewidth = 7, label = "guns")
+    axis1.plot(immigration, color = colors[3], linewidth = 7,label = "immigration")
+    axis1.plot(russia, color = colors[4], linewidth = 7, label = "russia")
+    axis1.plot(trade, color = colors[5], linewidth = 7, label = "trade")
+    axis1.plot(wall, color = colors[6], linewidth = 7, label = "wall")
+    axis1.plot(fbi, color = colors[7], linewidth = 7,label = "fbi")
+    axis1.plot(comey, color = colors[8], linewidth = 7,label = "comey")
+    axis1.plot(fake, color = colors[9], linewidth = 7,label = "fake")
+    axis1.plot(great, color = colors[10], linewidth = 7,label = "great")
+
     axis1.legend(prop = {'size': 20}, fontsize = 20)
     time_series_figure.savefig("{0}.png".format("time_series_word_frequency"), bbox_inches = 'tight') 
     
