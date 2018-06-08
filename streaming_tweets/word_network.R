5b674ffad96f41aafd8367ce6247297c36856a93
saverymax <saverymax@gmail.com>
2018-06-08 17:37:04 -0400

    stream

--- streaming_tweets/word_network.R
+++ streaming_tweets/word_network.R
@@ -15,15 +15,19 @@ library(ggraph)
 #tweet_dataframe = read.csv("~/Documents/Git/Twitter-Mining/streaming_tweets/data/cohen_fbi_raid_tsne.tsv", sep = '\t', stringsAsFactors = FALSE)
 
 # Liberal dataset:
-tweet_dataframe = read.csv("~/Documents/Git/Twitter-Mining/streaming_tweets/data/streaming_liberal_converted_tweets.tsv", sep = '\t', stringsAsFactors = FALSE)
+#tweet_dataframe = read.csv("~/Documents/Git/Twitter-Mining/streaming_tweets/data/streaming_liberal_converted_tweets.tsv", sep = '\t', stringsAsFactors = FALSE)
+
+# trump dataset:
+tweet_dataframe = read.csv("~/Documents/Git/Twitter-Mining/trump/data/topic_labels_sentiment_converted_tweets.tsv", sep = '\t', stringsAsFactors = FALSE)
 
 
 # make tibble dataframe:
 # Can do per topic or for whole data set
 
 # For a single topic:
-topic = 20
+topic = 9
 tweet_dataframe_reduced <- tweet_dataframe[tweet_dataframe$labels == topic, ] 
+
 # Coocuring words per tweet:
 text = data_frame(tweet = tweet_dataframe_reduced$filtered_text, id =  tweet_dataframe_reduced$filtered_text)
 
@@ -46,21 +50,28 @@ word_pairs <- text %>%
   pairwise_count(word, id, sort = TRUE, upper = FALSE)
 
 word_pairs
+cutoff = 4
 
 set.seed(1)
-plotFileName <- paste("~/Documents/Git/Twitter-Mining/streaming_tweets/data/topic_",topic ,".png",sep="")
+# If looking at specific topics:
+plotFileName <- paste("~/Documents/Git/Twitter-Mining/trump/data/topic_",topic ,".png",sep="")
+
+# if whole dataset:
+#plotFileName <- paste("~/Documents/Git/Twitter-Mining/trump/data/word_network_",cutoff,".png",sep="")
+
 png(filename = plotFileName)
 
 word_pairs %>% 
-  filter(n >= 3) %>%
+  filter(n >= cutoff) %>%
   graph_from_data_frame() %>%
   
   ggraph(layout = "fr") +
   geom_edge_link(aes(edge_alpha = n, edge_width = n), edge_colour = "cyan4") +
-  geom_node_point(size = 5) +
+  geom_node_point(size = 7) +
   geom_node_text(aes(label = name), repel = TRUE, 
                  point.padding = unit(0.2, "lines")) +
   theme_void() 
-wrtdev.off()  
+
+dev.off()  
 
 
